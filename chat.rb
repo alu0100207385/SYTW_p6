6e7689e5d3fb05bf87ebacdb7a7b1ea43c9e5368
Aaron Socas Gaspar <alu0100207385@ull.edu.es>
2014-11-15 17:02:17 +0000

    cambios varios en el diseño index

--- chat.rb
+++ chat.rb
@@ -12,6 +12,10 @@ get '/chat' do
    erb :chat
 end
 
+get '/help' do
+   erb :help
+end
+
 get '/send' do
   return [404, {}, "Not an ajax request"] unless request.xhr?
   chat << "#{request.ip} : #{params['text']}"
