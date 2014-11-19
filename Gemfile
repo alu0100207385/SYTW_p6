8f3ca5d7a80349f69946260dff194bf888da21f2
Aaron Socas Gaspar <alu0100207385@ull.edu.es>
2014-11-19 16:09:24 +0000

    texto sin colores

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ source 'https://rubygems.org'
 gem 'sinatra'
 gem 'sinatra-flash'
 gem 'thin'
-gem 'colorize'
+# gem 'colorize'
 
 group :development do
   gem 'sinatra-contrib'
--- chat.rb
+++ chat.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 require 'sinatra/flash'
 require 'sinatra/reloader' if development?
-require 'colorize'
+# require 'colorize'
 #set :port, 3000
 # set :environment, :production, :development
 
@@ -10,8 +10,8 @@ MAX = 20 #Usuarios maximos en la sala de chat
 chat = ['Bienvenido..']
 
 # users_color =['#F7FE2E','#A9F5A9','#58ACFA','#3A01DF','#610B5E','#DF3A01','#3B170B','#01DFD7','#FE2EC8','#FA5858', '#BF00FF','#585858','#B18904','#9FF781','#FF0040','#F6CECE','#585858','#0B0B3B','#1C1C1C','#F4FA58']
-color = [:black,:red,:green,:yellow,:blue,:magenta,:cyan,:white,:default,:light_black,
-         :light_red,:light_green,:light_yellow,:light_blue,:light_magenta,:light_cyan,:light_white]
+# color = [:black,:red,:green,:yellow,:blue,:magenta,:cyan,:white,:default,:light_black,:light_red,:light_green,:light_yellow,:light_blue,:light_magenta,:light_cyan,:light_white]
+
 enable :sessions
 set :session_secret, '*&(^#234a)'
 
@@ -41,7 +41,7 @@ get '/login' do
 	  nickname = nick
 	  users_on << nick
 	  session[:nickname] = nickname
-	  session[:color] = rand(color.size)
+# 	  session[:color] = rand(color.size)
 	  control = 0
 	  redirect '/chat'
    end
@@ -75,6 +75,7 @@ get '/send' do
 #    puts "-----------#{session[:nickname]}"
    return [404, {}, "Not an ajax request"] unless request.xhr?
    chat << "#{nick} : #{params['text']}"
+#    puts "Chuchu".colorize(color[rand(color.size)])
 #    chat << "#{request.ip} : #{params['text']}"
    nil
 end
